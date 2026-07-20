2d387ca003e0451566297f8d15d113f90be4807a
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-20 10:15:15 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.py
@@ -0,0 +1,20 @@
+# Last updated: 7/20/2026, 10:15:14 AM
+class Solution:
+    def romanToInt(self, s):
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+
+        total = 0
+        prev = 0
+
+        for char in reversed(s):
+            curr = roman_map[char]
+            if curr < prev:
+                total -= curr
+            else:
+                total += curr
+            prev = curr
+
+        return total
