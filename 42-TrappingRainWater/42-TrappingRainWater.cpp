92243222a8d6bdc535847a7c9dac768cdfff05b3
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-20 10:14:14 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -0,0 +1,27 @@
+// Last updated: 7/20/2026, 10:14:14 AM
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0, right = height.size() - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    water += leftMax - height[left];
+                ++left;
+            } else {
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    water += rightMax - height[right];
+                --right;
+            }
+        }
+
+        return water;
+    }
+};
