ddb41398269ce19d987b2bac9dafa75225ef70f0
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-20 10:14:39 +0530

    Create 31-NextPermutation

---
+++ 31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/20/2026, 10:14:39 AM
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int i = n - 2;
+
+        // Step 1: Find the first decreasing element from the end
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If such element is found, find the next larger element to swap
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+
+        // Step 3: Reverse the remaining elements to get the next permutation
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
