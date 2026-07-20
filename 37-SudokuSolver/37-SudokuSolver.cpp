2baad2abfd5f360dd44660b9f6d43ec9fa8d462e
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-20 10:14:26 +0530

    Create 37-SudokuSolver

---
+++ 37-SudokuSolver/37-SudokuSolver.cpp
@@ -0,0 +1,36 @@
+// Last updated: 7/20/2026, 10:14:25 AM
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+
+private:
+    bool solve(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; ++row) {
+            for (int col = 0; col < 9; ++col) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; ++num) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) return true;
+                            board[row][col] = '.'; // backtrack
+                        }
+                    }
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // board is filled
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        for (int i = 0; i < 9; ++i) {
+            if (board[row][i] == num || board[i][col] == num) return false;
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+            if (board[boxRow][boxCol] == num) return false;
+        }
+        return true;
+    }
+};
