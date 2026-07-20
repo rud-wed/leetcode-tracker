68d9807c391b71ce3c7813bdd9077aa776e7746d
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-20 10:15:13 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.py
@@ -0,0 +1,13 @@
+# Last updated: 7/20/2026, 10:15:12 AM
+class Solution:
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+            for word in strs[1:]:
+                if i >= len(word) or word[i] != char:
+                    return strs[0][:i]
+
+        return strs[0]
