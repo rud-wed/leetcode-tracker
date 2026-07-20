200c1b26fb43827f2722fad1264684b66122823e
rudhrapriya_s <rudhrasirini2006@gmail.com>
2026-07-20 10:13:41 +0530

    Create 47-PermutationsIi

---
+++ 47-PermutationsIi/47-PermutationsIi.cpp
@@ -0,0 +1,31 @@
+// Last updated: 7/20/2026, 10:13:41 AM
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> current;
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end());  // Sort to handle duplicates
+        backtrack(nums, used, current, result);
+        return result;
+    }
+
+    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& current, vector<vector<int>>& result) {
+        if (current.size() == nums.size()) {
+            result.push_back(current);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (used[i]) continue;
+            // Skip duplicates: only use the first occurrence unless the previous duplicate was used
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            current.push_back(nums[i]);
+            backtrack(nums, used, current, result);
+            current.pop_back();
+            used[i] = false;
+        }
+    }
+};
